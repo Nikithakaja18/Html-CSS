8a15733343ac5da9b3edcf422dca84d174d3b0e7
Kajanikitha <kajanikitha.24.csm@anits.edu.in>
2026-08-11 13:51:28 +0530

    Add HTML CSS JavaScript files

---
+++ project/script.js
@@ -0,0 +1,53 @@
+alert("Welcome to Student Management System");
+let students = [];
+
+function addStudent(){
+
+    let name = document.getElementById("studentName").value;
+
+    if(name==""){
+        alert("Enter student name");
+        return;
+    }
+
+    students.push(name);
+
+    document.getElementById("studentName").value="";
+
+    displayStudents();
+}
+
+
+function displayStudents(){
+
+    let list = document.getElementById("studentList");
+
+    list.innerHTML="";
+
+    students.forEach(function(student,index){
+
+        let li=document.createElement("li");
+
+        li.innerHTML =
+        (index+1)+". "+student+
+        `<button class="delete" onclick="deleteStudent(${index})">
+        Delete
+        </button>`;
+
+        list.appendChild(li);
+
+    });
+
+
+    document.getElementById("count").innerHTML = students.length;
+}
+
+
+
+function deleteStudent(index){
+
+    students.splice(index,1);
+
+    displayStudents();
+
+}
